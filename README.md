10731ae2b024b2507e3a1a1f3087d497405737b4
Julien Mihai <mihaijulien@gmail.com>
2023-12-21 17:39:47 +0200

    solution 2 RotateArray problem

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 |-|-|
 |[Solution](RemoveDuplicatesFromSortedArray.java)|[Remove Duplicates from Sorted Array](https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/)|
 |[Solution](BuySellStock.java)|[Remove Duplicates from Sorted Array](https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/)|
-|[Solution](RotateArray.java)|[Rotate Array](https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/)|
+|Solution: [1](RotateArray.java#L1)[2](RotateArray.java#L14)|[Rotate Array](https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/)|
--- RotateArray.java
+++ RotateArray.java
@@ -10,3 +10,34 @@ class Solution {
         }
     }
 }
+
+class Solution2 {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        // Input: nums = [1,2,3,4,5,6,7], k = 3
+        // Output: [5,6,7,1,2,3,4]
+        
+        // Reverse the entire array
+        // [7,6,5,4,3,2,1]
+        
+        // Reverse the first k elements
+        // [5,6,7,4,3,2,1]
+        
+        // Reverse the last nums.length - k elements
+        // [5,6,7,1,2,3,4]
+        
+        reverse(0, nums.length - 1, nums);
+        reverse(0, k - 1, nums);
+        reverse(k, nums.length - 1, nums);
+    }
+    
+    private void reverse(int start, int end, int[] nums){
+        while(start < end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
